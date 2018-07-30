c1baf808a8c636813935fe603e43956c8b10273b
Ashik <asibece@gmail.com>
2018-07-30 16:56:12 +0000

    new ones

---
+++ project1/README.md
@@ -0,0 +1,8 @@
+## project1 infrastructure
+
+Created 7/30/18 by admin. for project1
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ project1/variables.tf
@@ -0,0 +1,21 @@
+#####################################################################
+##
+##      Created 7/30/18 by admin. for project1
+##
+#####################################################################
+
+variable "TEST1_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "TEST1_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "TEST1_name" {
+  type = "string"
+  description = "Generated"
+}
+
